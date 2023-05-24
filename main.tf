d59e50042827cc395117735dcb12f75db5a7fece
Péter Molnár <xodax9898@gmail.com>
2023-05-25 01:10:20 +0200

    feat: created the write module

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ module "files" {
   content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
   name    = "FirstTask"
 }
+
+module "write" {
+  source     = "./modules/write"
+  answer_1  = var.answer_1
+  answer_2  = var.answer_2
+  answer_3  = var.answer_3
+  answer_4  = var.answer_4
+  answer_5  = var.answer_5
+}
